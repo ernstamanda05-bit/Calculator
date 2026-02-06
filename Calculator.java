f6ebf01501a33461901b543ec385a6e1ce078b7c
ernstamanda05-bit <ernstamanda05@gmail.com>
2026-02-06 14:00:26 -0500

    Add

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) {
         
+        // create Scanner to read user input
+        Scanner input = new Scanner(System.in);
+        
+        // ask user to enter two whole numbers
+        System.out.print("Enter the first whole number: ");
+        int num1 = input.nextInt();
+        
+        System.out.print("Enter the second whole number: ");
+        int num2 = input.nextInt();
+        
+        // add the numbers
+        int sum = num1 + num2;
+       
+        // print the result
+        System.out.println("The sum is: " + sum);
     }
 }
