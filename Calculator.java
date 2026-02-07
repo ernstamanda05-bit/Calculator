26c78c00771ed73b06199bbda2a5eb32dd8fcf64
ernstamanda05-bit <ernstamanda05@gmail.com>
2026-02-07 00:47:56 -0500

    Subtraction or addition

--- Calculator.java
+++ Calculator.java
@@ -2,21 +2,32 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-        
-        // create Scanner to read user input
+
         Scanner input = new Scanner(System.in);
-        
-        // ask user to enter two whole numbers
+
+        // ask user for two whole numbers
         System.out.print("Enter the first whole number: ");
         int num1 = input.nextInt();
-        
+
         System.out.print("Enter the second whole number: ");
         int num2 = input.nextInt();
-        
-        // add the numbers
-        int sum = num1 + num2;
-       
-        // print the result
-        System.out.println("The sum is: " + sum);
+
+        // ask user which operation to perform
+        System.out.print("Would you like to perform addition or subtraction? ");
+        String choice = input.next();
+
+        // perform operation based on user choice
+        if (choice.equalsIgnoreCase("addition")) {
+            System.out.println("You chose addition.");
+            System.out.println("Result: " + (num1 + num2));
+        } 
+        else if (choice.equalsIgnoreCase("subtraction")) {
+            System.out.println("You chose subtraction.");
+            System.out.println("Result: " + (num1 - num2));
+        } 
+        else {
+            System.out.println("Invalid option.");
+        }
     }
 }
+
